bd59e07ecd52f5a5c544bfac4375a26684d50358
Chris-M-Bennett <cmbennett1@hotmail.com>
2020-12-06 21:39:42 +0000

    Save and Load for settings added

---
+++ Assets/Scripts/Saving/SaveData.cs
@@ -0,0 +1,26 @@
+﻿using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Saving{
+    [System.Serializable]
+    public class SaveData
+    {
+       // public Slider brightnessSlider, volumeSlider, fovSlider;
+       // public Dropdown graphicQualityDrop;
+       // public Toggle fullScreenToggle;
+        public float brightness, volume, cameraFov;
+        public int quality;
+        public bool fullScreen;
+        public float[] playerPosition;
+    
+        public SaveData(SettingsScript settingsScript)
+        {
+            brightness = settingsScript.brightnessSlider.value;
+            volume = settingsScript.volumeSlider.value;
+            cameraFov = settingsScript.fovSlider.value;
+            quality = settingsScript.graphicQualityDrop.value;
+            fullScreen = settingsScript.fullScreenToggle.isOn;
+        }
+    }
+}
---
+++ Assets/Scripts/Saving/SerialiseSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UI;
+using UnityEngine;
+
+namespace Saving{
+    public static class SerialiseSystem
+    {
+    
+        public static void SaveData (SettingsScript settingsScript)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string path = Application.persistentDataPath + "/settingsData.did";
+            Debug.Log(path);
+            FileStream stream = new FileStream(path, FileMode.Create);
+        
+            SaveData data = new SaveData(settingsScript);
+        
+            formatter.Serialize(stream, data);
+            stream.Close();
+        }
+    
+        public static SaveData LoadData()
+        {
+            string path = Application.persistentDataPath + "/settingsData.did";
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                FileStream stream = new FileStream(path, FileMode.Open);
+            
+                SaveData data = formatter.Deserialize(stream) as SaveData;
+                stream.Close();
+            
+                return data;
+            }else
+            {
+                Debug.Log($"Save file not found in {path}");
+                return null;
+            }
+        }
+    }
+}
