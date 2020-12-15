6b10de2817caaaa6462633a248145bb5fdb789e5
Chris-M-Bennett <cmbennett1@hotmail.com>
2020-12-15 19:13:42 +0000

    Mannequin Puzzle Fix

---
+++ Assets/Scripts/Interaction/ReceiveObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interaction{
+    public class ReceiveObject : MonoBehaviour
+    {
+        [SerializeField][Tooltip("Version of object carried by player")]
+        private GameObject received;
+        [SerializeField][Tooltip("Version of object attached to this object")]
+        private GameObject attached;
+
+        private void Start()
+        {
+          attached.SetActive(false);  
+        }
+
+        private void OnCollisionEnter(Collision col)
+        {
+            if (col.gameObject == received)
+            {
+                Destroy(received);
+                attached.SetActive(true);
+            }
+        }
+    }
+}
